f16e3b9b6434f0b336131829368855203f0c8377
Finn Nielsen <fn@asseco.dk>
2022-06-15 15:27:26 +0200

    DDT-1108 - align TO and entities - fix dependencies

--- ovs-service/src/main/java/org/dcsa/ovs/Application.java
+++ ovs-service/src/main/java/org/dcsa/ovs/Application.java
@@ -2,10 +2,14 @@ package org.dcsa.ovs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("org.dcsa")
+// @EntityScan("org.dcsa")
+// @EnableJpaRepositories("org.dcsa")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
